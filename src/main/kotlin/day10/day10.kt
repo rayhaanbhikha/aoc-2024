b150bd35b8cdc00ff6f92f65dfddc8607f49a325
Rayhaan Bhikha <Rayhaanbhikha3@hotmail.com>
2024-12-21 21:11:20 +0000

    day 10 part 2 done

--- src/main/kotlin/day10/day10.kt
+++ src/main/kotlin/day10/day10.kt
@@ -2,7 +2,7 @@ package day10
 
 import utils.*
 
-fun score(startingCoord: Coord, grid: Grid<Int>): Int {
+fun score(startingCoord: Coord, grid: Grid<Int>, keepTrack: Boolean = false): Int {
     val startingCellValue = grid.cells.getOrNull(startingCoord.row)?.getOrNull(startingCoord.col) ?: return 0
     if (startingCellValue != 0) return 0
 
@@ -15,9 +15,16 @@ fun score(startingCoord: Coord, grid: Grid<Int>): Int {
 
         val cellValue = grid.cells.getOrNull(currentCoord.row)?.getOrNull(currentCoord.col) ?: continue
 
-        if (cellValue == 9 && !trailHeadsSeen.contains(currentCoord)) {
-            counter++
-            trailHeadsSeen.add(currentCoord)
+        if (cellValue == 9) {
+            when {
+                keepTrack -> when {
+                    !trailHeadsSeen.contains(currentCoord) -> {
+                        counter++
+                        trailHeadsSeen.add(currentCoord)
+                    }
+                }
+                else -> counter++
+            }
             continue
         }
 
@@ -27,7 +34,8 @@ fun score(startingCoord: Coord, grid: Grid<Int>): Int {
             currentCoord.cardinalNeighbours()
                 .filter { it.inValidRange(0..grid.maxRow, 0..grid.maxColumn) }
                 .mapNotNull { nextCoord ->
-                    val nextCellValue = grid.cells.getOrNull(nextCoord.row)?.getOrNull(nextCoord.col) ?: return@mapNotNull null
+                    val nextCellValue =
+                        grid.cells.getOrNull(nextCoord.row)?.getOrNull(nextCoord.col) ?: return@mapNotNull null
 
                     if (expectedNextSearchValue == nextCellValue) {
                         nextCoord
@@ -52,7 +60,7 @@ fun part1(): Int {
     val grid = input.toGrid { num -> if (num == ".") -2 else num.toInt() }
 
     grid.map {
-        score(it.coord, grid)
+        score(it.coord, grid, true)
     }.sum().also { println(it) }
 
 
@@ -60,5 +68,31 @@ fun part1(): Int {
 }
 
 fun part2(): Int {
+////    val input = """
+////        .....0.
+////        ..4321.
+////        ..5..2.
+////        ..6543.
+////        ..7..4.
+////        ..8765.
+////        ..9....
+////    """.trimIndent()
+//    val input = """
+//..90..9
+//...1.98
+//...2..7
+//6543456
+//765.987
+//876....
+//987....
+//    """.trimIndent()
+    val input = loadInput(10)
+
+    val grid = input.toGrid { num -> if (num == ".") -2 else num.toInt() }
+
+    grid.map {
+        score(it.coord, grid)
+    }.sum().also { println(it) }
+
     return 0
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,4 @@
 fun main() {
     day10.part1()
+    day10.part2()
 }
